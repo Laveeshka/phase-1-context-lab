45dbefee9b3a76958cf3b93208a684106de2598f
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-28 07:43:29 +1000

    completed lab

--- index.js
+++ index.js
@@ -21,3 +21,66 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(arr){
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arrayOfArrays){
+    return arrayOfArrays.map(arr => createEmployeeRecord(arr))
+}
+
+function createTimeInEvent(dateTime){
+    //YYYY-MM-DD HHMM
+    let [date, hour] = dateTime.split(" ");
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date 
+    })
+
+    return this
+}
+
+function createTimeOutEvent(dateTime){
+    //YYYY-MM-DD HHMM
+    let [date, hour] = dateTime.split(" ");
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date 
+    })
+
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    const {hour:hourIn} = this.timeInEvents.find(timeInEvent => timeInEvent.date === date)
+    const {hour:hourOut} = this.timeOutEvents.find(timeOutEvent => timeOutEvent.date === date)
+    const hoursWorked = (hourOut-hourIn)/100
+    return hoursWorked;
+}
+
+function wagesEarnedOnDate(matchedDate){
+    return this.payPerHour * hoursWorkedOnDate.call(this, matchedDate);
+}
+
+function findEmployeeByFirstName(collectionOfEmployees, firsNameString){
+    const foundEmployee = collectionOfEmployees.find(employee => employee.firstName === firsNameString);
+    return foundEmployee;
+}
+
+function calculatePayroll(collectionOfEmployees){
+    const payroll = collectionOfEmployees.reduce((acc, employeeRecord) => {
+        return acc + allWagesFor.call(employeeRecord)
+    }, 0);
+
+    return payroll;
+}
+
